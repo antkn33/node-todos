e17ec22342717dad1c624da84469015fd77ed6ee
Anthony Scott <antkn33@gmail.com>
2017-06-12 09:57:49 -0400

    add user route and token methods

--- server/server.js
+++ server/server.js
@@ -1,14 +1,14 @@
 // https://git.heroku.com/gentle-taiga-43088.git
 require('./config/config');
 
-const _ = require('lodash')
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
-var {user} = require('./models/user');
+var {User} = require('./models/user');
 
 var app = express();
 const port = process.env.PORT;
@@ -31,7 +31,7 @@ app.get('/todos', (req, res) => {
 	Todo.find().then((todos) => {
 		res.send({todos});
 	}, (e) => {
-		res.send(400).send(e);
+		res.status(400).send(e);
 	});
 });
 
@@ -69,7 +69,7 @@ app.patch('/todos/:id', (req, res) => {
 	var id = req.params.id;
 	var body = _.pick(req.body, ['text', 'completed']);
 
-	if(!ObjectID.isValid(id)) {
+	if (!ObjectID.isValid(id)) {
 		return res.status(404).send();
 	}
 
@@ -88,9 +88,24 @@ app.patch('/todos/:id', (req, res) => {
 		res.send({todo});
 	}).catch((e) => {
 		res.status(400).send();
-	});
+	})
 });
 
+// POST /users
+app.post('/users', (req, res) => {
+	var body = _.pick(req.body, ['email', 'password']);
+	var user = new User(body);
+
+	user.save().then(() => {
+		return user.generateAuthToken();	
+	}).then((token) => {
+		res.header('x-auth', token).send(user);
+	}).catch((e) => {
+		res.status(400).send(e);
+	})
+});
+
+//start server
 app.listen(port, () => {
 	console.log(`Started on port ${port}`);
 });
